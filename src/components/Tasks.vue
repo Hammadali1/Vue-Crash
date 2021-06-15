d3a231a206346ea26b558e731fc93250c95a72fb
ahammad <ahammad@nisum.com>
2021-06-15 17:27:57 +0500

    aSD

---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @dblclick="$emit('toggle-reminder', task.id)" :key="task.id" v-for="task in tasks">
+    <Task @delete-task="$emit('delete-task',task.id)" :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+export default {
+  name: "Tasks",
+  components: {
+    Task,
+  },
+  props: {
+    tasks: Array,
+  },
+  emits: ["delete-task",'toggle-reminder'],
+};
+</script>
